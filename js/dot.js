0d04b5ea10776da6ecb14503ac051a85cec48efe
Ddd0916 <bakbigdevil@gmail.com>
2020-10-27 20:01:13 +0800

    add menu

---
+++ js/dot.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $(function () {
+      $("#menu01").click(function () {
+        $("html,body").animate(
+          { scrollTop: $("#sec01").offset().top },
+          1000
+        );
+      });
+    });
+  
+  
+    $(function () {
+      $("#menu02").click(function () {
+        $("html,body").animate(
+          { scrollTop: $("#sec02").offset().top },
+          1000
+        );
+      });
+    });
+ 
+  
+    $(function () {
+      $("#menu03").click(function () {
+        $("html,body").animate(
+          { scrollTop: $("#sec03").offset().top },
+          1000
+        );
+      });
+    });
+  
+ 
+    $(function () {
+      $("#menu04").click(function () {
+        $("html,body").animate(
+          { scrollTop: $("#sec04").offset().top },
+          1000
+        );
+      });
+    });
+ });
