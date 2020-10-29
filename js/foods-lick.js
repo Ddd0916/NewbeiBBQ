65746a493ad2ca2f9bac2844a9dea9028f6aec3b
Ddd0916 <bakbigdevil@gmail.com>
2020-10-30 01:27:54 +0800

    add index add card

--- js/foods-lick.js
+++ js/foods-lick.js
@@ -12,3 +12,57 @@ $(".work-time2").slick({
 autoplaySpeed: 1500,
 // dots: true,
 });
+
+
+
+
+// $(".said-about-row").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+// autoplaySpeed: 2500,
+// dots: true,
+// });
+$('.said-about-row').slick({
+  dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
