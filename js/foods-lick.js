d515df1748c781de3b26014ebc9c33b027bf8ed5
Ddd0916 <bakbigdevil@gmail.com>
2020-10-30 02:26:12 +0800

    add index fix

--- js/foods-lick.js
+++ js/foods-lick.js
@@ -1,15 +1,80 @@
-$(".food").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-  autoplaySpeed: 1500,
+// $(".food").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 1500,
+//   dots: true,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//     settings: {
+//       slidesToShow: 2,
+//       slidesToScroll: 1,
+//       infinite: true,
+//       dots: true
+//     }}
+//   ]
+// });
+$('.food').slick({
   dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
+
 $(".work-time2").slick({
   slidesToShow: 2,
   slidesToScroll: 1,
   autoplay: true,
-autoplaySpeed: 1500,
+  autoplaySpeed: 1500,
+  responsive: [
+        {
+          breakpoint: 624,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }}
+      ]
+
 // dots: true,
 });
 
